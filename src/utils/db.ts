1c39086a1aa2a5aa0d43b70ba51eb95544074ec6
Abdul <up2dul@gmail.com>
2023-12-11 15:50:05 +0700

    feat: create db config and types file

---
+++ src/utils/db.ts
@@ -0,0 +1,14 @@
+import mysql from 'mysql2';
+import 'dotenv/config';
+
+const dbPool = mysql
+  .createPool({
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME,
+  })
+  .promise();
+
+export default dbPool;
