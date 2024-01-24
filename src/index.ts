7e4f3c36c4a272a8a526b93e1b6431a4b91a7bb9
Abdul <up2dul@gmail.com>
2024-01-24 23:57:39 +0700

    feat: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import express from 'express';
-import type { Application, Request, Response, NextFunction } from 'express';
+import type { Application, NextFunction, Response } from 'express';
+import cors from 'cors';
+
+import activityRoutes from './routes/activity';
+import todoRoutes from './routes/todo';
 
 const app: Application = express();
 const port = 3030;
 
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  const data = {
-    status: 'OK',
-    message: 'Hello express!',
-  };
-  res.status(200).send(data);
+app.use(express.json());
+app.use(cors());
+app.use((_, res: Response, next: NextFunction) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	next();
 });
+
+app.use('/activity-groups', activityRoutes);
+app.use('/todo-items', todoRoutes);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
